f0ed414b19522ba4e1ff981b14bdb6724db96f5d
hailey gaylor and micah zaayer <micah@>
2019-09-09 10:53:49 -0700

    add functionality to not allow feed if user has been eaten.

--- spec/date-spec.js
+++ spec/date-spec.js
@@ -1,1 +1,53 @@
- import {  } from './../src/bear-game.js';
+ import { HungryBear } from './../src/bear-game.js';
+
+ describe('HungryBear', function() {
+   let fuzzy
+
+   beforeEach(function() {
+     fuzzy  = new HungryBear("Fuzzy");
+     jasmine.clock().install();
+     fuzzy.setHunger();
+   });
+   afterEach(function() {
+     jasmine.clock().uninstall();
+   });
+
+   // it('should have a name and a food level of 10 when it is created', function() {
+   //   expect(fuzzy.name).toEqual("Fuzzy");
+   //   expect(fuzzy.foodLevel).toEqual(10);
+   // });
+   //
+   //
+   // it('should have a food level of 7 after 3001 milliseconds', function() {
+   //   jasmine.clock().tick(3001);
+   //   expect(fuzzy.foodLevel).toEqual(7);
+   // });
+   //
+   // it('should get very hungry if the food level drops below zero', function() {
+   //   fuzzy.foodLevel = 0;
+   //   expect(fuzzy.didYouGetEaten()).toEqual(true);
+   // });
+   //
+   // it('should get very hungry if 10 seconds pass without feeding', function() {
+   //   jasmine.clock().tick(10001);
+   //   expect(fuzzy.didYouGetEaten()).toEqual(true);
+   // });
+   //
+   // it('should have a food level of ten if it is fed', function() {
+   //   // let fuzzy = new HungryBear("Fuzzy");
+   //   jasmine.clock().tick(9001);
+   //
+   //   fuzzy.feed();
+   //   console.log(this.foodLevel);
+   //   expect(fuzzy.foodLevel).toEqual(10);
+   // });
+
+   it('should not let you feed bear if you are eaten', function () {
+    // let fuzzy = new HungryBear("Fuzzy");
+     jasmine.clock().tick(10001);
+     fuzzy.feed();
+     console.log(fuzzy.foodLevel);
+     expect(fuzzy.foodLevel).toEqual(0);
+   });
+
+ });
--- src/bear-game.js
+++ src/bear-game.js
@@ -1,1 +1,33 @@
 // Back-end
+export class HungryBear {
+
+  constructor(name) {
+    this.name = name;
+    this.foodLevel = 10;
+  }
+
+  setHunger() {
+    setInterval(() => {
+      this.foodLevel--;
+    }, 1000);
+  }
+  didYouGetEaten() {
+    if (this.foodLevel > 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+    //
+    // feed() {
+    //   this.foodLevel = 10;
+    // }
+
+  feed() {
+    if (this.foodLevel < 1) {
+      return false;
+    } else {
+      this.foodLevel = 10;
+    }
+  }
+}
