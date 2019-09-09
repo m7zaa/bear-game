d19a88bc117e1754705ec13f757f0192d3887150
hailey gaylor and micah zaayer <hailey@>
2019-09-09 11:36:30 -0700

    add partial ui.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,35 @@
 //front-end
+import $ from 'jquery';
+import {
+  HungryBear
+} from './bear-game';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css'
+
+
+$(document).ready(function(){
+  $("#startButton").click(function() {
+    event.preventDefault();
+    let bearName = $("#bearNameInput").val();
+
+    let newBear = new HungryBear(bearName);
+    console.log(newBear.foodLevel);
+    $("#currentLevel").text(newBear.foodLevel);
+    $(".foodLevel").show();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  });
+});
